fd954b07c9e767244e3042c4fdc0e6fa506f22c5
Prince Bhesaniya <126847549+PrinceBhesaniya28@users.noreply.github.com>
2025-05-19 13:46:16 +0530

    task 4 complete

--- components/footer/FooterAdmin.tsx
+++ components/footer/FooterAdmin.tsx
@@ -7,47 +7,30 @@ export default function Footer() {
     <div className="flex w-full flex-col items-center justify-between px-1 pb-8 pt-3 xl:flex-row">
       <p className="mb-4 text-center text-sm font-medium text-zinc-500 dark:text-zinc-400 sm:!mb-0 md:text-lg">
         <span className="mb-4 text-center text-sm text-zinc-500 dark:text-zinc-400 sm:!mb-0 md:text-sm">
-          ©{new Date().getFullYear()} Horizon AI Boilerplate. All Rights
-          Reserved.
+          ©{new Date().getFullYear()} Dialo Portal. All Rights Reserved.
         </span>
       </p>
       <div>
         <ul className="flex flex-wrap items-center gap-3 sm:flex-nowrap md:gap-10">
           <li>
-            <a
-              target="blank"
-              href="mailto:hello@simmmple.com"
-              className="text-sm font-medium text-zinc-500 hover:text-foreground dark:text-zinc-400"
-            >
+            <span className="text-sm font-medium text-zinc-500 dark:text-zinc-400">
               FAQs
-            </a>
+            </span>
           </li>
           <li>
-            <a
-              target="blank"
-              href="https://horizon-ui.com/"
-              className="text-sm font-medium text-zinc-500 hover:text-foreground dark:text-zinc-400"
-            >
+            <span className="text-sm font-medium text-zinc-500 dark:text-zinc-400">
               Privacy Policy
-            </a>
+            </span>
           </li>
           <li>
-            <a
-              target="blank"
-              href="https://horizon-ui.com/boilerplate"
-              className="text-sm font-medium text-zinc-500 hover:text-foreground dark:text-zinc-400"
-            >
+            <span className="text-sm font-medium text-zinc-500 dark:text-zinc-400">
               Terms & Conditions
-            </a>
+            </span>
           </li>
           <li>
-            <a
-              target="blank"
-              href="https://blog.horizon-ui.com/"
-              className="text-sm font-medium text-zinc-500 hover:text-foreground dark:text-zinc-400"
-            >
+            <span className="text-sm font-medium text-zinc-500 dark:text-zinc-400">
               Refund Policy
-            </a>
+            </span>
           </li>
         </ul>
       </div>
